c5fdbb13aa92ee939056325744a48df4c50355ec
Paul Pucciarelli <ppucciarelli@iown1.net>
2017-12-16 15:34:12 -0500

    Index rendering.

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ const Express = require('express');
 const fs = require('fs-extra');
 const http = require('http');
 const path = require('path');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
 
 const BABEL_RC = JSON.parse(
   fs.readFileSync(path.resolve(process.cwd(), '.babelrc'), 'utf-8')
 );
 
 const babel = require('babel-register')(BABEL_RC);
-const Index = require('./templates/Index');
+const config = require(path.resolve(process.cwd(), 'config'));
+const Index = require('./templates/Index').default;
 
 const express = new Express();
 const server = new http.Server(express);
@@ -19,3 +22,8 @@ express.get('*', (req, res, next) => {
   const html = ReactDOMServer.renderToString(index);
   res.send(`<!doctype html>${ html }`);
 });
+
+server.listen(config.PORT, (error) => {
+  if (error) console.error(error);
+});
+
--- backend/templates/index.js
+++ backend/templates/index.js
@@ -1,14 +1,16 @@
+import React from 'react';
+
 const Index = () => (
-  <html class="no-js" lang="en-us">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title>2018 Skeleton</title>
-  </head>
-  <body>
-    <div id="reactRoot" style={{ minHeight: '100vh' }} />
-  </body>
+  <html className="no-js" lang="en-us">
+    <head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <title>2018 Skeleton</title>
+    </head>
+    <body>
+      <div id="reactRoot" style={{ minHeight: '100vh' }} />
+    </body>
   </html>
 );
 
